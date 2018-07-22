aaaaec420f93a4c8b5ae1367f0b292b440608423
AugustJo <guangpuzhao@126.com>
2018-07-22 21:20:35 +0800

    增加结束GameOverscene的UI控制

--- Assets/Scripts/UI/GameOverUIController.cs
+++ Assets/Scripts/UI/GameOverUIController.cs
@@ -4,29 +4,67 @@ using UnityEngine.UI;
 
 public class GameOverUIController : MonoBehaviour {
 
-    private DataSaveController dataSaveController;
+    public static GameOverUIController Instance { get; private set; }
 
     public List<Text> groupScores;
+    public List<Image> groupImage;
+    public Image winnerImage;
+    public Text CountdownText;
+    // 倒计时时长
+    public float totalCountdownTime = 10f;
 
-	// Use this for initialization
-	void Start () {
-        dataSaveController = DataSaveController.Instance;
-        ShowScores();
+    private DataSaveController dataSaveController;
+
+    private void Awake()
+    {
+        Instance = this;
     }
-	
-	//// Update is called once per frame
-	//void Update () {
-		
-	//}
+    // Use this for initialization
+    void Start () {
+        dataSaveController = DataSaveController.Instance;
+        if(dataSaveController != null)
+            ShowScores();
+    }
+
+    //// Update is called once per frame
+    void Update()
+    {
+        if (totalCountdownTime > 0f)
+        {
+            totalCountdownTime -= Time.deltaTime;
+            UpdateCountdownTime(totalCountdownTime);
+        }
+        else
+        {
+            // TODO:切换回开始场景
+        }
+    }
 
     private void ShowScores()
     {
         int groupNumbers = dataSaveController.playerNumber / 2;
+        int winnerId = 0;
+        int winnerScores = 0;
         for (int i = 0; i < groupNumbers; ++i)
         {
             groupScores[i].gameObject.SetActive(true);
-            groupScores[i].text = dataSaveController.scores[i].ToString();
+            groupScores[i].text = "得分："+ dataSaveController.scores[i].ToString();
+            // TODO:切换为显示用户头像
+            groupImage[i].color = dataSaveController.groupColor[i];
+            if(winnerScores < dataSaveController.scores[i])
+            {
+                winnerScores = dataSaveController.scores[i];
+                winnerId = i;
+            }
         }
+        winnerImage.color = dataSaveController.groupColor[winnerId];
+    }
+
+    // 更新倒计时
+    private void UpdateCountdownTime(float remainTimes)
+    {
+        int remainTimeInteger = (int)remainTimes;
+        CountdownText.text = remainTimeInteger.ToString();
     }
     
 }
