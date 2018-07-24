7b4a4589594b34feda205e8846bc77d351aa1db8
xinnjie <xinnjie31@gmail.com>
2018-07-24 11:03:20 +0800

    SkillTiem增加需要占领属性，占领道具的时间请使用`TimeToCaptime`进行设置 Todo: 这个类需要重构，还没有和具体道具的功能整合到一起

--- Assets/Scripts/Item/SkillItem.cs
+++ Assets/Scripts/Item/SkillItem.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
 using UnityEngine;
 
 public class SkillItem : MonoBehaviour {
@@ -11,28 +12,70 @@ public class SkillItem : MonoBehaviour {
     // 要实例化的控制对象
     public GameObject skillController;
 
+    // 获取这个道具需要的时间
+    public float timeToCapture;
+    // 现在是否有玩家正在占取这个道具
+    private bool beingCaptured;
+    // 正在获取道具的玩家是谁
+    private GroupPlayer capturingGroupPlayer;
+    // 已经占取的时间
+    private float capturingTime;
+
 	// Use this for initialization
 	void Start () {
         if (skillController == null)
             Debug.Log("放置物品");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
+    // Todo 如果有多个玩家同时站在道具的占领区，可能会出现问题
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.gameObject.name);
-        if(other.tag == "Player")
+        if(!beingCaptured && other.tag == "Player")
         {
+            beingCaptured = true;
+            capturingTime = 0.0f;
             GroupPlayer groupPlayer = other.GetComponent<GroupPlayer>();
+            capturingGroupPlayer = groupPlayer;
             if (groupPlayer.CatchItem(this))
                 Destroy(gameObject);
         }
     }
-    
+
+    private void OnTriggerStay(Collider other)
+    {
+        /*
+         * 只要当正在占领的玩家处在占领道具的区域内，就对占领时间进行计时
+         * 到达占领时间后让玩家取得该道具，如果玩家不能取得该道具，则重置道具状态
+         */
+        if (beingCaptured)
+        {
+            capturingTime += Time.deltaTime;
+            if (capturingTime > timeToCapture)
+            {
+                if (capturingGroupPlayer.CatchItem(this))
+                {
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    beingCaptured = false;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (beingCaptured)
+        {
+            beingCaptured = false;
+            capturingTime = 0.0f;
+            capturingGroupPlayer = null;
+        }
+
+    }
+
     public PigSkillController GetPigSkillController()
     {
         return Instantiate(skillController.GetComponent<PigSkillController>());
