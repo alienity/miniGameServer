3ba0c74796072553c79d7f5ecdb4f3ff0bd0b8d3
alienity <578409434@qq.com>
2018-07-25 23:35:58 +0800

    增加蓄力

---
+++ Assets/Scripts/GameControllUtility/messages/ChargeSkillMsg.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ChargeSkillMsg : MessageBase
+{
+
+    public int gId;
+    public int uId;
+    public float chargeStartTime;   // 蓄力开始时刻
+    public float chargeCurrentTime; // 蓄力当前时刻
+    public bool chargeReturn;
+
+    public ChargeSkillMsg(int gId, int uId, float chargeStartTime, float chargeCurrentTime, bool chargeReturn)
+    {
+        this.gId = gId;
+        this.uId = uId;
+        this.chargeStartTime = chargeStartTime;
+        this.chargeCurrentTime = chargeCurrentTime;
+        this.chargeReturn = chargeReturn;
+    }
+
+    public ChargeSkillMsg() { }
+
+}
---
+++ Assets/Scripts/GameControllUtility/messages/RushSkillMag.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RushSkillMag : MessageBase
+{
+
+    public int gId;
+    public int uId;
+    public bool skill;
+
+    public RushSkillMag(int gId, int uId, bool skill)
+    {
+        this.gId = gId;
+        this.uId = uId;
+        this.skill = skill;
+    }
+
+    public RushSkillMag() { }
+
+}
