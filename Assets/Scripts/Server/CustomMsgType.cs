d217c641f1fe5e6eb5e32e3c170316fc832e9511
xinnjie <xinnjie31@gmail.com>
2018-07-26 14:13:58 +0800

    震动功能

--- Assets/Scripts/Server/CustomMsgType.cs
+++ Assets/Scripts/Server/CustomMsgType.cs
@@ -6,8 +6,9 @@ public class CustomMsgType
     public const short Confirm = MsgType.Highest + 2;
     public const short RoleState = MsgType.Highest + 3;  // 发送、接收某一个角色当前可用状态的信息
     public const short ClientChange = MsgType.Highest + 4; // 通知手机端切换界面
-    
+
     public const short GroupControll = MsgType.Highest + 5; // 发送和接受队伍和角色的控制信息
     public const short GroupState = MsgType.Highest + 6; // 发送到手机端状态信息
 
+    public const short AdvanceControl = MsgType.Highest + 10; // 震动等消息调用
 }
