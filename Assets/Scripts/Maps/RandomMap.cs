50f4a2b85dbf6a2854e6af425e24b3506ea2e6ae
WuWenQiangqwq <wu13868142015@icloud.com>
2018-08-06 05:08:34 +0800

    Add files via upload

---
+++ Assets/Scripts/Maps/RandomMap.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RandomMap : MonoBehaviour {
+
+	private int[] pos;
+    public GameObject cloneObject;
+    public int cloneNum;
+    private int clonedNum;
+    private System.Random rand;
+    void Start () {
+        pos = new int[10];
+        clonedNum = 0;
+        for (int i = 0; i < 9; i++)
+            pos[i] = i;
+        rand = new System.Random();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (clonedNum <cloneNum)
+        {
+            int rpos = rand.Next(9 - clonedNum);
+            int x = pos[rpos];
+            setMap(x);
+            clonedNum++;
+
+            pos[rpos] += pos[9 - clonedNum];
+            pos[9 - clonedNum] = pos[rpos] - pos[9 - clonedNum];
+            pos[rpos] = pos[rpos] - pos[9 - clonedNum];
+            //x;
+        }
+	}
+    private void setMap(int idx)
+    {
+        Instantiate(cloneObject,new Vector3(idx / 3 + 2.5f, 0, idx % 3 + 2.5f), Quaternion.identity);
+    }
+    public void getObject()
+    {
+        clonedNum--;
+    }
+}
