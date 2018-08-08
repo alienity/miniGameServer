b197c7c5313b4d0e05d4620bf4212c0a35b36000
WuWenQiangqwq <wu13868142015@icloud.com>
2018-08-08 14:50:19 +0800

    添加蹦的特效

--- Assets/Scripts/Maps/FallStone.cs
+++ Assets/Scripts/Maps/FallStone.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class FallStone : MonoBehaviour {
 
+    public RandomMap map_;
     public ParticleSystem explosion;
     public ParticleSystem Trail;
+    public ParticleSystem markCircle;
+    private bool readyPlay = true;
     private void Start()
     {
+        readyPlay = true;
         Instantiate(Trail, transform);
+        Vector3 circlePos = transform.position;
+        circlePos.y = 10;
+        /*markCircle =*/
+            Destroy(Instantiate(markCircle,circlePos,new Quaternion(0,90,0,0)),5);
     }
     private void OnCollisionEnter(Collision collision)
     {
-        explosion = Instantiate(explosion,transform.position,Quaternion.identity) as ParticleSystem;
-
-        Destroy(this.gameObject, 0.1f);
+        Debug.Log("line explosion"+" "+collision.gameObject.tag);
+        if (collision.gameObject.tag != "FallStone")
+        {
+            Destroy(Instantiate(explosion, transform.position, Quaternion.identity), 1);
+            Destroy(this.gameObject, 0.2f);
+            map_.getObject();
+        }
     }
 }
