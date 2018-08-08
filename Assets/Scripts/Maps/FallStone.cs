93b86b5f36620e3a0e594f1e38567e1128f6bf62
WuWenQiangqwq <wu13868142015@icloud.com>
2018-08-08 10:21:19 +0800

    Add files via upload

---
+++ Assets/Scripts/Maps/FallStone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallStone : MonoBehaviour {
+
+    public ParticleSystem explosion;
+    public ParticleSystem Trail;
+    private void Start()
+    {
+        Instantiate(Trail, transform);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        explosion = Instantiate(explosion,transform.position,Quaternion.identity) as ParticleSystem;
+
+        Destroy(this.gameObject, 0.1f);
+    }
+}
