aed237dc180b98ce430ac3b35d7295691dccb3c0
WuWenQiangqwq <wu13868142015@icloud.com>
2018-08-09 15:09:59 +0800

    可在setMap写对其他Tag的处理

--- Assets/Scripts/Maps/RandomMap.cs
+++ Assets/Scripts/Maps/RandomMap.cs
@@ -1,50 +1,65 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class RandomMap : MonoBehaviour {
+public class RandomMap : MonoBehaviour
+{
 
-	private int[] pos;
+    private int[] pos;
+
+    private System.Random rand;
     public GameObject cloneObject;
+
     public int cloneNum;
     private int clonedNum;
-    private System.Random rand;
+
     public List<Transform> reBorns;
+
     public float reBornCD;
     private float reTimes;
-    void Start () {
-        reTimes = -1;
+
+    void Start()
+    {
+        reTimes = -1; clonedNum = 0;
+
         pos = new int[10];
-        clonedNum = 0;
-        for (int i = 0; i < 9; i++)
-            pos[i] = i;
         rand = new System.Random();
+
+        for (int i = 0; i < 9; i++) pos[i] = i;
     }
-	
-	void Update () {
+
+    void Update()
+    {
         if (reTimes < 0)
         {
-            clonedNum = 0;
-            int len = reBorns.Count;
-            while (clonedNum < cloneNum)
+
+            if (clonedNum < cloneNum)
             {
-                int rpos = rand.Next(cloneNum);
-                int x = pos[rpos];
-                setMap(x);
+                //在有效区里随机
+                int len = reBorns.Count;
+                int rpos = rand.Next(len - clonedNum-1);
                 clonedNum++;
-                Debug.Log(x);
+                setMap(pos[rpos]);
+                //交换随机点到末尾
+                int backPos = len - clonedNum-1;
+                pos[rpos] ^= pos[backPos];
+                pos[backPos] ^= pos[rpos];
+                pos[rpos] ^= pos[backPos];
             }
-            reTimes = reBornCD;
+            
         }
         else reTimes -= Time.deltaTime;
-	}
+    }
     private void setMap(int idx)
     {
-        //Instantiate(cloneObject,reBorns[idx]);
-        Vector3 p = reBorns[idx].position;
-        Instantiate(cloneObject, p, Quaternion.identity);
+        if(cloneObject.tag =="FallStone")
+        {
+            Instantiate(cloneObject, reBorns[idx].position,reBorns[idx].rotation);
+            getObject();
+        }
     }
+    //保持地图上 随机的物品数，由物品的 触发器函数调用
     public void getObject()
     {
         clonedNum--;
