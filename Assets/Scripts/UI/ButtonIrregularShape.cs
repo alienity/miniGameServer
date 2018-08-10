605295bfd34801b5309dc7f609931af3ccda0aa5
AugustJo <guangpuzhao@126.com>
2018-08-10 18:35:37 +0800

    更新Server端选人界面UI

---
+++ Assets/Scripts/UI/ButtonIrregularShape.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonIrregularShape : MonoBehaviour {
+
+    private Image image;
+	// Use this for initialization
+	void Start () {
+        image = this.GetComponent<Image>();
+        image.alphaHitTestMinimumThreshold = 0.05f;
+
+    }
+
+}
--- Assets/Scripts/UI/RoleChoosingUIController.cs
+++ Assets/Scripts/UI/RoleChoosingUIController.cs
@@ -17,41 +17,57 @@ public class RoleChoosingUIController : MonoBehaviour {
     // 进度显示分数（1/8）
     public Text FractionalText;
     // ChooseRoleScene的Button
-    public Button P1Pengu;
-    public Button P1Pig;
-    public Button P2Pengu;
-    public Button P2Pig;
-    public Button P3Pengu;
-    public Button P3Pig;
-    public Button P4Pengu;
-    public Button P4Pig;
-    public Sprite PenguHeadAltern;
-    public Sprite PenguHeadSelect;
-    public Sprite PenguHeadLock;
-    public Sprite PigHeadAltern;
-    public Sprite PigHeadSelect;
-    public Sprite PigHeadLock;
+    //public Button P1Pengu;
+    //public Button P1Pig;
+    //public Button P2Pengu;
+    //public Button P2Pig;
+    //public Button P3Pengu;
+    //public Button P3Pig;
+    //public Button P4Pengu;
+    //public Button P4Pig;
+    //public Sprite PenguHeadAltern;
+    //public Sprite PenguHeadSelect;
+    //public Sprite PenguHeadLock;
+    //public Sprite PigHeadAltern;
+    //public Sprite PigHeadSelect;
+    //public Sprite PigHeadLock;
+
+    [System.Serializable]
+    public struct PlayerIcone
+    {
+        public Image HeadSelect;
+        public Image ConfirmMaskImage;
+        public Text NameText;
+        public Image ConfirmFrameImage;
+        public Image ChooseFrameImage;
+        public Image CrownImage;
+        public Button button;
+    }
+    public List<PlayerIcone> playerIcone = new List<PlayerIcone>();
     private Button[] buttons;
     // 进入GameScene前的倒计时
     public Text CountDown;
+    // Title 请选择你的英雄和战队
+    public Text Title; 
     
     // 改名字的部分
     public List<Text> playerNames;
     private List<string> defaultNames = new List<string>()
     {
-        "唐吉诃鹅 - P1",
-        "罗齐南猪 - P1",
-        "唐吉诃鹅 - P2",
-        "罗齐南猪 - P2",
-        "唐吉诃鹅 - P3",
-        "罗齐南猪 - P3",
-        "唐吉诃鹅 - P4",
-        "罗齐南猪 - P4",
+        "企鹅-P1",
+        "猪猪-P1",
+        "企鹅-P2",
+        "猪猪-P2",
+        "企鹅-P3",
+        "猪猪-P3",
+        "企鹅-P4",
+        "猪猪-P4"
     };
-    
+
     private void Start()
     {
         InitArray();
+        Title.gameObject.SetActive(true);
         CountDown.gameObject.SetActive(false);
 
         // 初始场景切换
@@ -75,50 +91,71 @@ public class RoleChoosingUIController : MonoBehaviour {
     private void InitArray()
     {
         buttons = new Button[8];
-        buttons[0] = P1Pengu;
-        buttons[1] = P1Pig;
-        buttons[2] = P2Pengu;
-        buttons[3] = P2Pig;
-        buttons[4] = P3Pengu;
-        buttons[5] = P3Pig;
-        buttons[6] = P4Pengu;
-        buttons[7] = P4Pig;
 
-        for(int i = 0; i < 4; ++i )
+        int i = 0;
+        foreach (PlayerIcone pIcone in playerIcone)
         {
-            //button.image.color = Color.white;D:\github_MiniGame\miniGameServer\Assets\Resource\Textures\ChooseRoleSceneICon
-            buttons[2 * i].GetComponent<Image>().sprite = PenguHeadAltern;
-            buttons[2 * i + 1].GetComponent<Image>().sprite = PigHeadAltern;
+            // 主机端不需要button
+            buttons[i].interactable = false;
+            buttons[i++] = pIcone.button;
         }
+        //buttons[0] = P1Pengu;
+        //buttons[1] = P1Pig;
+        //buttons[2] = P2Pengu;
+        //buttons[3] = P2Pig;
+        //buttons[4] = P3Pengu;
+        //buttons[5] = P3Pig;
+        //buttons[6] = P4Pengu;
+        //buttons[7] = P4Pig;
+
+        //for(int i = 0; i < 4; ++i )
+        //{
+        //    //button.image.color = Color.white;D:\github_MiniGame\miniGameServer\Assets\Resource\Textures\ChooseRoleSceneICon
+        //    buttons[2 * i].GetComponent<Image>().sprite = PenguHeadAltern;
+        //    buttons[2 * i + 1].GetComponent<Image>().sprite = PigHeadAltern;
+        //}
 
     }
 
     // todo 到时候在这里为 button 设置效果
     public void SetButtonRoleAvailable(int gid, int uid)
     {
-
-        buttons[gid * 2 + uid].GetComponent<Image>().sprite = (uid == 0) ? PenguHeadAltern : PigHeadAltern;
-        //buttons[gid*2 + uid].interactable = true;
-        //buttons[gid * 2 + uid].image.color = DataSaveController.Instance.groupColor[gid];
-        //buttons[gid * 2 + uid].image.color = Color.white;
-
+        playerIcone[gid * 2 + uid].HeadSelect.gameObject.SetActive(false);
+        playerIcone[gid * 2 + uid].NameText.text = defaultNames[gid * 2 + uid];
+        playerIcone[gid * 2 + uid].ChooseFrameImage.gameObject.SetActive(false);
+        //playerIcone[gid * 2 + uid].CrownImage.gameObject.SetActive(false);
+        //BarImages[gid].gameObject.SetActive(false);
+        //buttons[gid * 2 + uid].interactable = true;
+        // 旧版本
+        //buttons[gid * 2 + uid].GetComponent<Image>().sprite = (uid == 0) ? PenguHeadAltern : PigHeadAltern;
     }
 
     // todo 到时候在这里为 button 设置效果
     public void SetButtonRoleSelected(int gid, int uid)
     {
-        //buttons[gid*2 + uid].interactable = false;
-        //buttons[gid * 2 + uid].image.color = DataSaveController.Instance.groupColor[gid];
-        buttons[gid * 2 + uid].GetComponent<Image>().sprite = (uid == 0) ? PenguHeadSelect : PigHeadSelect;
+        playerIcone[gid * 2 + uid].HeadSelect.gameObject.SetActive(true);
+        playerIcone[gid * 2 + uid].ConfirmMaskImage.gameObject.SetActive(false);
+        //playerIcone[gid * 2 + uid].NameText.text = playerName;
+        playerIcone[gid * 2 + uid].ConfirmFrameImage.gameObject.SetActive(false);
+        //playerIcone[gid * 2 + uid].ChooseFrameImage.gameObject.SetActive(true);
+        //playerIcone[gid * 2 + uid].CrownImage.gameObject.SetActive(false);
+        // 旧版本
+        // buttons[gid * 2 + uid].GetComponent<Image>().sprite = (uid == 0) ? PenguHeadSelect : PigHeadSelect;
     }
 
     public void SetButtonRoleLocked(int gid, int uid)
     {
-        buttons[gid * 2 + uid].GetComponent<Image>().sprite = (uid == 0) ? PenguHeadLock : PigHeadLock;
+        playerIcone[gid * 2 + uid].HeadSelect.gameObject.SetActive(true);
+        playerIcone[gid * 2 + uid].ConfirmFrameImage.gameObject.SetActive(true);
+        //playerIcone[gid * 2 + uid].ChooseFrameImage.gameObject.SetActive(false);
+        //playerIcone[gid * 2 + uid].CrownImage.gameObject.SetActive(true);
+        // 旧版本
+        //buttons[gid * 2 + uid].GetComponent<Image>().sprite = (uid == 0) ? PenguHeadLock : PigHeadLock;
     }
 
     public void CountDownTextSetActive()
     {
+        Title.gameObject.SetActive(false);
         CountDown.gameObject.SetActive(true);
     }
 
@@ -131,7 +168,9 @@ public class RoleChoosingUIController : MonoBehaviour {
     {
         for (int i = 0; i < 8; i++)
         {
-            playerNames[i].text = role2Name.ContainsKey(i) ? role2Name[i] : defaultNames[i];
+            playerIcone[i].NameText.text = role2Name.ContainsKey(i) ? role2Name[i] : defaultNames[i];
+            // 旧版本
+            //playerNames[i].text = role2Name.ContainsKey(i) ? role2Name[i] : defaultNames[i];
         }
     }
 }
